ef8c0314c21a23ec53ea14d5c2d7fb1aca70c12a
Robert Nicholson <bob@busapp.com>
2021-12-11 16:26:27 -0600

    Initial commit 2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          A New paragraph by Bob, again 2.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
